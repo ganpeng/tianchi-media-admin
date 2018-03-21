b188b254aec8504b227447ce4b31de35cb808aa0
xiaofei.liu <liuxiaofei2010S@163.com>
2018-03-21 14:52:18 +0800

    update the structure of project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+// webpack入口文件
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入ElementUI全部组件，也可以部分引入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'babel-polyfill'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/page/login/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data () {
+            return {
+                msg: 'h5-admin'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    div{
+        width: 200px;
+    }
+</style>
