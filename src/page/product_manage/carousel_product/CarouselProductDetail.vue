5a54ed8ebd1c303133d84ca6f41605379f6cf289
xiaofei.liu <liuxiaofei2010S@163.com>
2018-10-26 14:38:13 +0800

    [BO]1.编写产品管理中的轮播频道包和直播回看包详情页面
    Summary: 1.编写产品管理中的轮播频道包和直播回看包详情页面
    
    Test Plan:
    Build & run，示图如下：
    {F12891}
    {F12892}
    
    Reviewers: binhong.wu, peng.gan
    
    Reviewed By: peng.gan
    
    Differential Revision: http://pha.tianchimedia.com/D1734

--- src/page/product_manage/carousel_product/CarouselProductDetail.vue
+++ src/page/product_manage/carousel_product/CarouselProductDetail.vue
@@ -1,6 +1,11 @@
 <!--轮播频道包详情组件-->
 <template>
     <div>
+        <custom-breadcrumb
+            v-bind:breadcrumbList="[
+            {name:'产品管理'},
+            {name:'轮播频道包详情'}]">
+        </custom-breadcrumb>
         <div class="vice-block" id="basic-info">
             <h3 class="block-vice-title">轮播频道包信息</h3>
             <el-card>
@@ -8,16 +13,6 @@
                     <li><span>产品包类别</span><label>轮播频道包</label></li>
                     <li><span>产品包名称</span><label>{{productInfo.name}}</label></li>
                     <li><span>产品包简介</span><label>{{productInfo.description}}</label></li>
-                    <li>
-                        <span>包含类型</span>
-                        <label>
-                            <el-tag v-for="(item, index) in productInfo.categoryList"
-                                    :key="index"
-                                    type="info">
-                                {{item.name}}
-                            </el-tag>
-                        </label>
-                    </li>
                     <li>
                         <span>产品包状态</span>
                         <label>
@@ -27,6 +22,94 @@
                     </li>
                 </ul>
             </el-card>
+            <div class="vice-block">
+                <h3 class="block-vice-title">产品包内轮播频道列表</h3>
+                <el-table
+                    :data="channelList"
+                    header-row-class-name="common-table-header"
+                    row-class-name=channel-row
+                    border
+                    style="width: 100%">
+                    <el-table-column
+                        align="center"
+                        width="70px"
+                        label="序号">
+                        <template slot-scope="scope">
+                            <label>{{scope.$index + 1}}</label>
+                        </template>
+                    </el-table-column>
+                    <el-table-column
+                        prop="no"
+                        width="80px"
+                        align="center"
+                        label="编号">
+                    </el-table-column>
+                    <el-table-column
+                        prop="innerName"
+                        width="200px"
+                        align="center"
+                        label="名称">
+                    </el-table-column>
+                    <el-table-column
+                        align="center"
+                        width="120px"
+                        label="类别">
+                        <template slot-scope="scope">
+                            <label>{{scope.row.typeList | jsonJoin('name')}}</label>
+                        </template>
+                    </el-table-column>
+                    <el-table-column
+                        align="center"
+                        prop="multicastIp"
+                        label="组播地址">
+                    </el-table-column>
+                    <el-table-column
+                        align="center"
+                        width="80px"
+                        prop="multicastPort"
+                        label="端口号">
+                    </el-table-column>
+                    <!--tsId-->
+                    <el-table-column
+                        align="center"
+                        prop="tsId"
+                        label="tsId">
+                        <template slot-scope="scope">
+                            <label>{{scope.row.tsId ? scope.row.tsId : '------'}}</label>
+                        </template>
+                    </el-table-column>
+                    <!--serviceId-->
+                    <el-table-column
+                        align="center"
+                        prop="serviceId"
+                        label="serviceId">
+                        <template slot-scope="scope">
+                            <label>{{scope.row.serviceId ? scope.row.serviceId : '------'}}</label>
+                        </template>
+                    </el-table-column>
+                    <el-table-column
+                        align="center"
+                        prop="pushServer"
+                        label="所属服务器">
+                        <template slot-scope="scope">
+                            <label>{{scope.row.pushServer ? scope.row.pushServer : '------'}}</label>
+                        </template>
+                    </el-table-column>
+                    <el-table-column
+                        align="center"
+                        width="80px"
+                        label="状态">
+                        <template slot-scope="scope">
+                            <i class="status-normal" v-if="scope.row.visible">正常</i>
+                            <i class="status-abnormal" v-else>禁播</i>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
+            <div id="operate-list">
+                <el-button type="primary" class="page-main-btn" @click="editInfo">编辑信息</el-button>
+                <el-button @click="toProductList" class="page-main-btn">返回列表页</el-button>
+            </div>
         </div>
     </div>
 </template>
@@ -38,20 +121,31 @@
         data() {
             return {
                 productInfo: {
-                    name: '',
-                    description: '',
-                    categoryList: [],
+                    name: '刘德华剧场包',
+                    description: '这个是刘德华剧场包',
                     visible: true
-                }
+                },
+                channelList: []
             };
         },
-        methods: {}
+        methods: {
+            editInfo() {
+                this.$router.push({
+                    name: 'EditCarouselProduct', params: this.$route.params
+                });
+            },
+            toProductList() {
+                this.$router.push({name: 'ProductList'});
+            }
+        }
     };
 </script>
 
 <style lang="scss" scoped>
 
     #basic-info {
+        position: relative;
+        padding-top: 80px;
         .el-card {
             width: 600px;
             ul {
@@ -82,6 +176,11 @@
                     }
                 }
             }
+        }
+        #operate-list {
+            position: absolute;
+            right: 0px;
+            top: 10px;
         }
     }
 
--- src/page/product_manage/look_back_product/LookBackProductDetail.vue
+++ src/page/product_manage/look_back_product/LookBackProductDetail.vue
@@ -14,16 +14,6 @@
                     <li><span>产品包名称</span><label>{{productInfo.name}}</label></li>
                     <li><span>产品包简介</span><label>{{productInfo.description}}</label></li>
                     <li>
-                        <span>包含类型</span>
-                        <label>
-                            <el-tag v-for="(item, index) in productInfo.categoryList"
-                                    :key="index"
-                                    type="info">
-                                {{item.name}}
-                            </el-tag>
-                        </label>
-                    </li>
-                    <li>
                         <span>产品包状态</span>
                         <label>
                             <i class="status-normal" v-if="productInfo.visible">已上架</i>
@@ -32,6 +22,69 @@
                     </li>
                 </ul>
             </el-card>
+            <div class="vice-block">
+                <h3 class="block-vice-title">产品包内直播回看列表</h3>
+                <el-table
+                    :data="channelList"
+                    header-row-class-name="common-table-header"
+                    row-class-name=channel-row
+                    border
+                    style="width: 100%">
+                    <el-table-column
+                        align="center"
+                        width="70px"
+                        label="序号">
+                        <template slot-scope="scope">
+                            <label>{{scope.$index + 1}}</label>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="code" align="center" width="120px" label="直播频道编号"></el-table-column>
+                    <el-table-column prop="no" align="center" width="140px" label="直播频道展示编号"></el-table-column>
+                    <el-table-column prop="innerName" align="center" width="120px" label="直播频道名称">
+                        <template slot-scope="scope">
+                    <span class="ellipsis two">
+                        {{scope.row.innerName}}
+                    </span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="name" align="center" width="120px" label="直播频道展示名">
+                        <template slot-scope="scope">
+                    <span class="ellipsis two">
+                        {{scope.row.name}}
+                    </span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="type" width="120px" align="center" label="频道类别">
+                        <template slot-scope="scope">
+                    <span class="ellipsis two">
+                        {{scope.row.typeList | getTypeName}}
+                    </span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="multicastIp" width="150px" align="center" label="频道IP"></el-table-column>
+                    <el-table-column prop="multicastPort" width="100px" align="center" label="频道端口"></el-table-column>
+                    <el-table-column prop="pushServer" align="center" label="所属服务器"></el-table-column>
+                    <el-table-column align="center" label="是否录制回看">
+                        <template slot-scope="scope">
+                            {{scope.row.record ? '是' : '否'}}
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="videoPid" align="center" label="videoPid">
+                        <template slot-scope="scope">
+                            {{scope.row.videoPid | padEmpty}}
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="audioPid" align="center" label="audioPid">
+                        <template slot-scope="scope">
+                            {{scope.row.audioPid | padEmpty}}
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
+            <div id="operate-list">
+                <el-button type="primary" class="page-main-btn" @click="editInfo">编辑信息</el-button>
+                <el-button @click="toProductList" class="page-main-btn">返回列表页</el-button>
+            </div>
         </div>
     </div>
 </template>
@@ -43,20 +96,31 @@
         data() {
             return {
                 productInfo: {
-                    name: '',
-                    description: '',
-                    categoryList: [],
+                    name: 'CCTV1回看包',
+                    description: '这个是CCTV1回看包',
                     visible: true
-                }
+                },
+                channelList: []
             };
         },
-        methods: {}
+        methods: {
+            editInfo() {
+                this.$router.push({
+                    name: 'EditLookBackProduct', params: this.$route.params
+                });
+            },
+            toProductList() {
+                this.$router.push({name: 'ProductList'});
+            }
+        }
     };
 </script>
 
 <style lang="scss" scoped>
 
     #basic-info {
+        position: relative;
+        padding-top: 80px;
         .el-card {
             width: 600px;
             ul {
@@ -87,6 +151,11 @@
                     }
                 }
             }
+        }
+        #operate-list {
+            position: absolute;
+            right: 0px;
+            top: 10px;
         }
     }
 
